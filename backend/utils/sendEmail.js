a771121f020e699d828ed5bd7ffd16726ef93f61
itzh4r5h <h4r5h.c0d3s@gmail.com>
2025-11-03 18:15:19 +0530

    added port and secure true

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,28 +1,27 @@
-const nodemailer = require('nodemailer')
+const nodemailer = require("nodemailer");
 
+const sendEmail = async (options) => {
+  const { email, subject, message } = options;
 
-const sendEmail = async (options)=>{
-    const {email,subject,message} = options
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    service: process.env.SMTP_SERVICE,
+    port:  process.env.SMTP_PORT, 
+    secure: true,
+    auth: {
+      user: process.env.GMAIL_ID,
+      pass: process.env.GMAIL_PASS,
+    },
+  });
 
-    const transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        service: process.env.SMTP_SERVICE,
-        auth: {
-            user: process.env.GMAIL_ID,
-            pass: process.env.GMAIL_PASS
-        }
-    })
+  const mailOptions = {
+    from: `shopzilla <${process.env.GMAIL_ID}>`,
+    to: email,
+    subject,
+    text: message,
+  };
 
-    const mailOptions = {
-        from: `shopzilla <${process.env.GMAIL_ID}>`,
-        to: email,
-        subject,
-        text: message
-    }
+  await transporter.sendMail(mailOptions);
+};
 
-    await transporter.sendMail(mailOptions)
-}
-
-
-
-module.exports = sendEmail
+module.exports = sendEmail;
