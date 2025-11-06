92627533d2b99de692fb6a796bcf8437ff8259da
itzh4r5h <h4r5h.c0d3s@gmail.com>
2025-11-06 06:32:34 +0530

    added color prop and set the outline none

--- frontend/src/components/buttons/FillButton.jsx
+++ frontend/src/components/buttons/FillButton.jsx
@@ -1,6 +1,6 @@
-export const FillButton = ({ name,icon=false,type='',size='text-xl',padding='px-2 py-1' }) => {
+export const FillButton = ({ name,icon=false,type='',size='text-xl',padding='px-2 py-1',clr='bg-black' }) => {
   return (
-    <button type={type} className={`flex justify-center items-center gap-2 w-full ${size} ${padding} font-bold border-2 border-black rounded-md bg-black text-white active:bg-white active:text-black transition-colors duration-200 capitalize`}>
+    <button type={type} className={`flex justify-center items-center gap-2 w-full ${size} ${padding} font-bold border-2 border-black rounded-md ${clr} text-white active:bg-white active:text-black transition-colors duration-200 capitalize outline-none`}>
      {icon ? (
         <>
           {icon}
